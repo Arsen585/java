400ee23b0c53dc0fa352a8666fde6091de603eb6
Arsen <baktbekovarsen82@gmail.com>
2026-01-26 12:56:21 +0500

    lab1

---
+++ lab 1/StudentInfo.java
@@ -0,0 +1,32 @@
+// Имя файла должно совпадать с именем класса
+// Файл: StudentInfo.java
+public class StudentInfo {
+
+    // Метод main — точка входа в Java-программу
+    public static void main(String[] args) {
+
+        // Объявление и инициализация переменной для хранения ФИО студента
+        String fullName = "Arsen ";
+
+        // Объявление переменной для хранения номера группы
+        String group = "ИС 24-12";
+
+        // Объявление переменной для хранения образовательной программы
+        String program = "Информационные системы";
+
+        String  lastName = "Baktbekov"
+
+        // Вывод заголовка в консоль
+        System.out.println("=== Информация о студенте ===");
+
+        // Вывод ФИО студента
+        System.out.println("ФИО: " + fullName);
+
+        // Вывод группы студента
+        System.out.println("Группа: " + group);
+
+        // Вывод образовательной программы
+        System.out.println("ОП/Специальность: " + program);
+
+    }
+}
---
+++ lab 2/main.java
@@ -0,0 +1,5 @@
+public  class Helloworld{
+    public  static void  nain(Sring[] args){
+        )
+    }
+}
